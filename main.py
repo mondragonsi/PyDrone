ff168905506d74e4c5c80b53c7a2de21aa88ce07
Bruno V. Mondragon <mondragon.si@gmail.com>
2023-01-30 12:58:01 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+drone.connect()
+
+print(drone.get_battery())
+
+drone.takeoff()
+
+drone.send_rc_control(0, 0, 0, 0)
+sleep(2)
+drone.send_rc_control(0, 0, 0, 0)
+drone.land()
